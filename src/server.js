de9e2b58997c07f8e09d1aab4c33a661ba44eeca
Septian Wiguna Bamba <septianwigunabamba@gmail.com>
2022-08-31 06:17:19 +0800

    fix bugs host value

--- src/server.js
+++ src/server.js
@@ -4,8 +4,13 @@ const Hapi = require('@hapi/hapi');
 const init = async () => {
   const server = Hapi.server({
     port: 5000,
-    host: 'localhost',
-  });
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+  routes: {
+    cors: {
+      origin: ['*'],
+    },
+  },
+});
  
  
   await server.start();
